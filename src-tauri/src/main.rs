453260eb6e354c7caed6a4e1c76823fe3b14199f
bibibobibabi <37201767+bibibobibabi@users.noreply.github.com>
2026-07-26 16:16:59 +0800

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod commands;
-
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_sql::Builder::default().build())
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_store::Builder::default().build())
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![
-            commands::read_text_file,
-            commands::decode_text_file,
-            commands::extract_pdf_text,
-            commands::export_data,
-            commands::import_data,
-            commands::get_system_fonts,
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    moyue_reader_lib::run();
 }
